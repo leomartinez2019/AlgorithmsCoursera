0d78752c6c2e303fd48572f3af0fd5269e7c2a86
Leonardo Martinez <lowenhard@yahoo.com>
2018-07-26 06:58:40 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+** Coursera Algorithms, part 1
+
+About this Course
+
+This course covers the essential information that every serious programmer
+needs to know about algorithms and data structures, with emphasis on
+applications and scientific performance analysis of Java implementations.
+Part I covers elementary data structures, sorting, and searching algorithms.
+Part II focuses on graph- and string-processing algorithms.
+
+Taught by Robert Sedgewick and Kevin Wayne
---
+++ quickfind.py
@@ -0,0 +1,42 @@
+# Quick find algorithm from Coursera course
+
+class Quicko(object):
+    def __init__(self, size):
+        self.d = list(range(size))
+        self.size = size
+
+    def connected(self, p, q):
+        'find if two elements are connected'
+        if self.d[p] == self.d[q]:
+            return True
+        return False
+
+    def union(self, p, q):
+        'merge two elements by equaling their values'
+        if not self.connected(p, q):
+            p_indx = self.d[p]
+            q_indx = self.d[q]
+            for indx in range(self.size):
+                if self.d[indx] == p_indx:
+                    self.d[indx] = q_indx
+
+# testing function union
+
+def test2():
+    'testing both operations / not unit testing here :('
+    k = Quicko(10)
+    k.union(4,3)
+    print(k.d)
+    k.union(3,8)
+    print(k.d)
+    k.union(6,5)
+    print(k.d)
+    k.union(9,4)
+    print(k.d)
+    k.union(2,1)
+    print(k.d)
+    assert k.connected(8,9) == True, "Test failed"
+    assert k.connected(7,8) == False, "Test failed"
+    print("Test passed")
+
+test2()
