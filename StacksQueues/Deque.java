2d1f116b93cf6642c84adf560a56800838b5fa12
leomartinez2019 <lowenhard@yahoo.com>
2019-09-11 07:41:23 -0500

    double deque improved

--- StacksQueues/Deque.java
+++ StacksQueues/Deque.java
@@ -1,4 +1,5 @@
 import java.util.Iterator;
+import java.util.Random;
 import java.util.NoSuchElementException;
 
 //public class Deque<Item> implements Iterable<Item> {
@@ -28,36 +29,49 @@ public class Deque<Item> {
 
     // add the item to the back
     public void addLast(Item item) {
-      StdOut.println("------addItem Start---------");
-      StdOut.println("tail: " + tail);
-      StdOut.println("head: " + head);
-      StdOut.println("it.length: " + it.length);
       it[tail] = item;
       N++;
       if (N == it.length) resize(it.length * 2);
-      tail = (head + N);;
-      //tail = (tail + 1) % it.length;
-      StdOut.println("tail: " + tail);
-      StdOut.println("N: " + N);
-      StdOut.println("------addItem End---------");
+      tail = (head + N) % it.length;
     }
 
     // add the item to the front
     public void addFirst(Item item) {
-      if (isEmpty()) {
-        it[head] = item;
-      } else {
-        head = (head + 1) % it.length;
-        it[head] = item;
-      }
       N++;
-      if (N == it.length) resize(it.length * 2);
+      if (N == it.length) {
+        resize(it.length * 2);
+        tail = head + N;
+      }
+      if (it[head] != null) {
+        head = (head - 1) % it.length;
+        if (head < 0)  head += it.length;
+        tail = (head + N) % it.length;
+      }
+      StdOut.print("head(addFirst): ");
+      StdOut.println(head);
+      StdOut.print("tail(addFirst): ");
+      StdOut.println(tail);
+      it[head] = item;
     }
 
     // remove and return the item from the last
-    //public Item removeLast() {
-      
-    //}
+    public Item removeLast() {
+      if (N == 0) throw new NoSuchElementException("nada aquí...");
+      StdOut.print("tail: ");
+      StdOut.println(tail);
+      tail = (tail - 1) % it.length;
+      if (tail < 0)  tail += it.length; 
+      StdOut.print("tail: ");
+      StdOut.println(tail);
+      Item item = it[tail];
+      it[tail] = null;
+      N--;      
+      if (N > 0 && N == it.length / 4)
+        resize(it.length / 2);
+      StdOut.print("tail: ");
+      StdOut.println(tail);
+      return item;
+    }
 
     // remove and return the item from the front
     public Item removeFirst() {
@@ -79,27 +93,16 @@ public class Deque<Item> {
     //}
 
     private void resize(int capacity) {
-      StdOut.println("------Resize start --------");
       Item[] copy = (Item[]) new Object[capacity];
       int c = 0;
       int j;
-      //int temp = tail < head ? tail + N : tail;
       for (int i = head; i < head + N; i++) {
         j = i % it.length;
         copy[c++] = it[j];
-        //i = (i + 1) % it.length; 
       }
       head = 0;
-      //tail = (head + N); //% it.length;
-      //tail = (head + N); //% it.length;
+      tail = head + N;
       it = copy;
-      StdOut.print("head: ");
-      StdOut.println(head);
-      StdOut.print("tail: ");
-      StdOut.println(tail);
-      StdOut.print("capacity: ");
-      StdOut.println(capacity);
-      StdOut.println("------Resize end --------");
     }
 
     public String toString() {
@@ -141,14 +144,70 @@ public class Deque<Item> {
 
     public static void test2() {
       Deque<String> d = new Deque<>();
-      d.addFirst("Papa");
       d.addLast("queso");
+      d.addFirst("Papa");
+      d.addLast("coco");
+      d.addLast("zapote");
+      d.addFirst("yuca");
+      d.addFirst("arroz");
+      d.addFirst("aceite");
+      d.addFirst("ñame");
+      //d.addLast("corozo");
       StdOut.println(d);
 
     }
 
+    public static void test3() {
+      Deque<String> d = new Deque<>();
+      d.addFirst("Papa");
+      StdOut.println(d);
+      d.addLast("coco");
+      StdOut.println(d);
+      d.removeLast();
+      StdOut.println(d);
+      d.addLast("zapote");
+      StdOut.println(d);
+      d.addFirst("yuca");
+      StdOut.println(d);
+      d.removeLast();
+      StdOut.println(d);
+      d.addFirst("arroz");
+      StdOut.println(d);
+      d.addLast("corozo");
+      StdOut.println(d);
+    }
+
+    // Test reading from a text file
+    // Text file like: green yellow blue ^ $ pink 
+    // addFirst = "^", addLast = "$", removeFirst = "^-", removeLast = "$-"
+    public static void test4() {
+      Random num = new Random();
+      int randNum;
+      Deque<String> d = new Deque<>();
+      while (!StdIn.isEmpty()) {
+        String s = StdIn.readString();
+        if (s.equals("^")) {
+          StdOut.println(d.removeFirst());
+        }
+        else if (s.equals("$")) {
+          StdOut.println(d.removeLast());
+        }
+        else {
+          randNum = num.nextInt(100);
+          if (randNum < 50) {
+            d.addFirst(s);
+          }
+          else {
+            d.addLast(s);
+          }
+        }
+      }
+    }
+
     public static void main(String[] args) {
-      test2();
+      //test1();
+      //test2();
+      test4();
     }
 
 }
