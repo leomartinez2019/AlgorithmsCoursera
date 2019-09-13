4791eb72cfb1e028eae0055f74c627c4ec430c6a
leomartinez2019 <lowenhard@yahoo.com>
2019-09-12 19:46:16 -0500

    add randomized queue and permutation client

---
+++ StacksQueues/Permutation.java
@@ -0,0 +1,12 @@
+public class Permutation {
+   public static void main(String[] args) {
+     int valor = Integer.parseInt(args[0]);
+     RandomizedQueue<String> r = new RandomizedQueue<>();
+     while (!StdIn.isEmpty()) {
+       String s = StdIn.readString();
+       r.enqueue(s);
+     }
+     for (int k = 0; k < valor; k++) StdOut.println(r.sample());
+     //StdOut.println(r);
+   }
+}
---
+++ StacksQueues/RandomizedQueue.java
@@ -0,0 +1,119 @@
+import java.lang.IllegalArgumentException;
+import java.lang.UnsupportedOperationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    int head = 0;
+    int tail = 0;
+
+    Item[] q;
+    int N = 0;
+
+    // construct an empty randomized queue
+    public RandomizedQueue() {
+      q = (Item[]) new Object[1];
+    }
+
+    // is the randomized queue empty?
+    public boolean isEmpty() {
+      return N == 0;
+    }
+
+    // return the number of items on the randomized queue
+    public int size() {
+      return N;
+    }
+
+    // add the item
+    public void enqueue(Item item) {
+      if (item == null) throw new IllegalArgumentException("no null arguments allowed");
+      q[tail] = item;
+      N++;
+      if (N == q.length) resize(q.length * 2);
+      tail = (head + N) % q.length;
+    }
+
+    // remove and return a random item
+    public Item dequeue() {
+      if (N == 0) throw new NoSuchElementException("nothing here...");
+      Item item = q[head];
+      q[head] = null;
+      head = (head + 1) % q.length;
+      N--;
+      if (N > 0 && N == q.length / 4) {
+        resize(q.length / 2);
+        tail = head + N;
+      }
+      return item;
+    }
+
+    // return a random item (but do not remove it)
+    public Item sample() {
+      int num = StdRandom.uniform(N);
+      return q[num + head];
+    }
+
+    private void resize(int capacity) {
+      Item[] copy = (Item[]) new Object[capacity];
+      int c = 0;
+      int j;
+      for (int i = head; i < head + N; i++) {
+        j = i % q.length;
+        copy[c++] = q[j];
+      }
+      head = 0;
+      tail = head + N;
+      q = copy;
+    }
+
+    // return an independent iterator over items in random order
+    public Iterator<Item> iterator() {
+      return new ListIterator();
+    }
+
+    public class ListIterator implements Iterator<Item> {
+      private int indx = head;
+      public boolean hasNext() {
+        return q[(indx) % q.length] != null;
+      }
+      public Item next() {
+        if (q[indx] == null) throw new NoSuchElementException();
+        Item item = q[indx];
+        indx = (indx + 1) % q.length;
+        return item;
+      }
+      public void remove() {
+        throw new UnsupportedOperationException();
+      }
+    }
+
+    public String toString() {
+      String res = "";
+      for (int i = 0; i < q.length; i++) {
+        if (q[i] == null)  res += "Null";
+        else res += q[i];
+        res += "-";
+      }
+      return res;
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+      RandomizedQueue<String> r = new RandomizedQueue<>();
+      r.enqueue("aa");
+      r.enqueue("bb");
+      r.enqueue("cc");
+      r.enqueue("dd");
+      r.enqueue("ee");
+      r.enqueue("ff");
+      StdOut.println(r);
+      for (int k = 0; k < 20; k++){
+        StdOut.print(r.sample());
+        StdOut.print("-");
+      }
+      StdOut.println("");
+      for (String elem : r) StdOut.println(elem);
+    }
+
+}
