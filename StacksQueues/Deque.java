30d900131770880ab48c5eeaf3220416c0a7aeec
leomartinez2019 <lowenhard@yahoo.com>
2019-09-15 17:43:55 -0500

    improve name convention

--- StacksQueues/Deque.java
+++ StacksQueues/Deque.java
@@ -10,7 +10,7 @@ public class Deque<Item> implements Iterable<Item> {
     private int tail = 0;
 
     private Item[] it;
-    private int N = 0;
+    private int numElems = 0;
 
     // construct an empty deque
     public Deque() {
@@ -19,12 +19,12 @@ public class Deque<Item> implements Iterable<Item> {
 
     // is the deque empty?
     public boolean isEmpty() {
-      return N == 0;
+      return numElems == 0;
     }
 
     // return the number of items on the deque
     public int size() {
-      return N;
+      return numElems;
     }
 
     // add the item to the back
@@ -32,8 +32,8 @@ public class Deque<Item> implements Iterable<Item> {
       if (item == null) throw new IllegalArgumentException("no null arguments allowed");
       it[tail++] = item;
       if (tail == it.length) tail = 0;
-      N++;
-      if (N == it.length) resize(it.length * 2);
+      numElems++;
+      if (numElems == it.length) resize(it.length * 2);
     }
 
     // add the item to the front
@@ -42,30 +42,30 @@ public class Deque<Item> implements Iterable<Item> {
       head--;
       if (head < 0) head += it.length;
       it[head] = item;
-      N++;
-      if (N == it.length) resize(it.length * 2);
+      numElems++;
+      if (numElems == it.length) resize(it.length * 2);
     }
 
     // remove and return the item from the last
     public Item removeLast() {
-      if (N == 0) throw new NoSuchElementException("nothing here...");
+      if (numElems == 0) throw new NoSuchElementException("nothing here...");
       tail--;
       if (tail < 0)  tail += it.length; 
       Item item = it[tail];
       it[tail] = null;
-      N--;      
-      if (N > 0 && N == it.length / 4) resize(it.length / 2);
+      numElems--;  
+      if (numElems > 0 && numElems == it.length / 4) resize(it.length / 2);
       return item;
     }
 
     // remove and return the item from the front
     public Item removeFirst() {
-      if (N == 0) throw new NoSuchElementException("nada aquí...");
+      if (numElems == 0) throw new NoSuchElementException("nada aquí...");
       Item item = it[head];      
       it[head++] = null;
       if (head == it.length) head = 0;
-      N--;
-      if (N > 0 && N == it.length / 4) resize(it.length / 2);
+      numElems--;
+      if (numElems > 0 && numElems == it.length / 4) resize(it.length / 2);
       return item;
     }
 
@@ -94,23 +94,13 @@ public class Deque<Item> implements Iterable<Item> {
       Item[] copy = (Item[]) new Object[capacity];
       int c = 0;
       int j;
-      for (int i = head; i < head + N; i++) {
+      for (int i = head; i < head + numElems; i++) {
         j = i % it.length;
         copy[c++] = it[j];
       }
       head = 0;
-      tail = head + N;
+      tail = head + numElems;
       it = copy;
-    }
-
-    public String toString() {
-      String res = "";
-      for (int i = 0; i < it.length; i++) {
-        if (it[i] == null)  res += "Null";
-        else res += it[i];
-        res += "-";
-      }
-      return res;
     }
 
     // unit testing (required)
